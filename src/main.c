680e43eecfa3d3986d898ef798a98a75f9b935f2
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-04-16 14:55:03 +0200

    Added the main file and made it possible to compile and run using CMake.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+/*
+// Created by Wouter on 16/04/2024.
+*/
+
+#include "main.h"
+#include "stdio.h"
+
+int main(){
+    printf("hello world");
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Wouter on 16/04/2024.
+//
+
+#ifndef WOUTCLOUD_MAIN_H
+#define WOUTCLOUD_MAIN_H
+
+#endif //WOUTCLOUD_MAIN_H
