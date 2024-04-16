480270368e243bb401b08d01259a2b2b74c410ac
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-04-16 15:55:27 +0200

    Initialisation of winsock2.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,27 @@
 /*
-// Created by Wouter on 16/04/2024.
+// Created by Wouter Jacobs.
 */
 
-#include "main.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 
 int main(){
-    printf("hello world");
+    int initialisation_result;
+    WSADATA wsa_data;
+
+    // Initialization
+
+    initialisation_result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    if (initialisation_result != 0) {
+        printf("WSAStartup failed: %d\n", initialisation_result);
+        return 1;
+    }else{
+        printf("init succesfull");
+    }
+
+
     return 0;
 }
