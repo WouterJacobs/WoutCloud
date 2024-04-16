f8b9628c6577ca918431759f8c18527881f723d8
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-04-16 16:18:48 +0200

    Added the initialization and creation of sockets.

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@
 #include <ws2tcpip.h>
 #include <iphlpapi.h>
 
+#define DEFAULT_PORT "8081"
+
 int main(){
 
     //Initializes the Winsock library for network communication.
@@ -19,7 +21,42 @@ int main(){
         printf("WSAStartup failed: %d\n", initialisation_result);
         return 1;
     }else{
-        printf("init succesfull");
+        printf("Winsock2 init succesfull\n");
+    }
+
+
+    /*
+     * Creation of Sockets.
+     * using: - ipv4
+     *        - TCP protocol
+     * Port = 8081
+     */
+    struct addrinfo *result = NULL, *poiter = NULL, hints;
+
+    ZeroMemory(&hints, sizeof (hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    initialisation_result = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if (initialisation_result != 0) {
+        printf("getaddrinfo failed: %d\n", initialisation_result);
+        WSACleanup();
+        return 1;
+    }else{
+        printf("Adress init succesfull\n");
+    }
+
+    SOCKET ListeningSocket;
+    ListeningSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListeningSocket == INVALID_SOCKET) {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }else{
+        printf("Socket init succesfull\n");
     }
 
 
