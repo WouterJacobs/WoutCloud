35163a26226ba1b667972f948f17a5359993f7e3
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-04-16 16:30:11 +0200

    Added socketbinding and refactoring of variable names.

--- src/main.c
+++ src/main.c
@@ -48,9 +48,9 @@ int main(){
         printf("Adress init succesfull\n");
     }
 
-    SOCKET ListeningSocket;
-    ListeningSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (ListeningSocket == INVALID_SOCKET) {
+    SOCKET listening_socket;
+    listening_socket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (listening_socket == INVALID_SOCKET) {
         printf("Error at socket(): %ld\n", WSAGetLastError());
         freeaddrinfo(result);
         WSACleanup();
@@ -60,5 +60,17 @@ int main(){
     }
 
 
+    initialisation_result = bind(listening_socket, result->ai_addr, (int)result->ai_addrlen);
+    if (initialisation_result == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(listening_socket);
+        WSACleanup();
+        return 1;
+    }else{
+        printf("Binding socket to address and port succesfull\n");
+    }
+
+    freeaddrinfo(result);
     return 0;
 }
